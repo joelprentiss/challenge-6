546199784f19b965b78cc0cf9d59ad1e8b128aaa
Joel Prentiss <joel.prentiss89@gmail.com>
2022-07-09 22:20:00 -0500

    final commit for grade

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 var weatherSearchBtn = document.querySelector("#weather-search");
 var geoLocateApi= `http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=5&appid={76dea1d2eaa53c39fea214a799bab840}`;
-var weatherApiKey='76dea1d2eaa53c39fea214a799bab840';
-var forcastApiCall= `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid={76dea1d2eaa53c39fea214a799bab840}`
 var userInput = document.querySelector('#search-input');
 
 
-var weatherSearch= function(){
+var locationSearch= function(){
     fetch(geoLocateApi)
     .then (function(response){
         return response.json();
@@ -13,34 +11,36 @@ var weatherSearch= function(){
     .then(function(data){
         console.log(data)
         for (var i=0; i<data.length; i++){
-            var latitude= data[i].lat
-            var longitude=data[i].lon
-        }
-    .then (function(){
+             latitude= data[i].lat
+             longitude=data[i].lon
+        }})}
+
+ var weatherSearch=     
+        (function(){
         fetch(weatherApiCall)
         .then (function(response){
-                return response.json();
+                return response.json()}
         .then (function(data){
          for (var i=0; i<data.length; i++){
-         var currentWeather= doctument.createElement('');
-         var fiveDayForcast = document.createElement('');
-                    };
-                });
-            }
-            );
-        });  
-    });
-};
+         var currentWeather= doctument.createElement('forecast-day');
+         var fiveDayForcast = document.createElement('5day-forecast');
+                    }
+                })
+            )}
+            
+        );  
+    
+ var forcastApiCall= `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid={76dea1d2eaa53c39fea214a799bab840}`
+
+
 
 function searchHistory(){
     var prevSearch =JSON.parse(localStorage.getItem('Previous Search'));
     then (function(){
-        document.createElement('')
+        document.createElement('btn-secondary');
     })
-}
-
+};
 
 localStorage.setItem('Previous Search', userInput);
-window.onload=function(){
-weatherSearchBtn.addEventListener('click', weatherSearch)};
+weatherSearchBtn.addEventListener('click', weatherSearch);
 console.log(geoLocateApi);
